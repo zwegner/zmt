bef578d92e44942dee585d51845aa85ddce91d5b
Zach Wegner <zwegner@gmail.com>
2019-10-24 01:07:29 -0500

    Split lua iteration of lines into its own coroutine, separate from highlighting

--- zmt.lua
+++ zmt.lua
@@ -11,8 +11,9 @@ defs:close()
 --------------------------------------------------------------------------------
 
 -- Iterate through the given piece tree, starting at the given offset
-function iter_pieces(tree, byte_offset, line_offset)
+function iter_nodes(tree, byte_offset, line_offset)
     byte_offset = byte_offset or 0
+    line_offset = line_offset or 0
     return coroutine.wrap(function()
         local iter = ffi.new('meta_iter_t[1]')
         local node = zmt.iter_start(iter, tree, byte_offset, line_offset)
@@ -20,8 +21,42 @@ function iter_pieces(tree, byte_offset, line_offset)
             local l = node.leaf
             -- end is a keyword. Oh well, it's the proper variable name
             local data = ffi.string(l.chunk_data + l.start, l['end'] - l.start)
-            coroutine.yield(iter[0].start_offset.byte, data)
+            coroutine.yield(iter[0], node, data)
             node = zmt.iter_next(iter)
+        end
+    end)
+end
+
+-- Iterate through pieces, broken up by lines. We yield successive tuples of
+-- the form (line_number, byte_offset, is_end, piece), where is_end marks
+-- pieces that are at the end of their respective lines
+function iter_lines(tree, line_start, line_end)
+    return coroutine.wrap(function()
+        -- Start a piece iterator at the start line
+        local line = line_start
+        for iter, node, piece in iter_nodes(tree, 0, line_start) do
+            -- Chop up this piece into lines. We only do the scan the exact
+            -- number of times needed
+            local offset = iter.start_offset.byte
+            for i = 0, node.nl_count - 1 do
+                -- Cut off any part after a newline
+                local idx = piece:find('\n')
+                --assert idx ~= nil
+                part = piece:sub(1, idx - 1)
+                piece = piece:sub(idx + 1)
+                coroutine.yield(line, offset, true, part)
+                offset = offset + idx
+                line = line + 1
+                if line > line_end then
+                    break
+                end
+            end
+            if line > line_end then
+                break
+            end
+
+            -- Yield the last part of this piece
+            coroutine.yield(line, offset, false, piece)
         end
     end)
 end
@@ -96,55 +131,48 @@ function show_highlight(tree, ast, query)
     local q_end = -1
 
     -- Iterate through all lines in the file
-    for line = 0, tree.root.nl_count do
+    local last_line = -1
+    for line, offset, is_end, piece in iter_lines(tree, 0,
+            tree.root.nl_count - 1) do
         -- Line number display
-        io.stdout:write(HL_LINE_NB_FMT:format(line + 1) .. cur_hl)
+        if line ~= last_line then
+            io.stdout:write(HL_LINE_NB_FMT:format(line + 1) .. cur_hl)
+            last_line = line
+        end
 
-        -- Iterate through the pieces of this line, adding highlighting according
-        -- to the tree-sitter captures and the HL_TYPE table above
-        for offset, piece in iter_pieces(tree, 0, line) do
-            -- Cut off any part after a newline
-            local idx = piece:find('\n')
-            if idx ~= nil then
-                piece = piece:sub(1, idx - 1)
-            end
-
-            -- Chop up this piece into parts that share the same highlight
-            while #piece > 0 do
-                -- Jump the cursor forward until we know the next match
-                if offset > q_end then
-                    q_start, q_end = next_capture()
-                -- This piece starts or ends highlight. Output the escape code,
-                -- and one character of source so we make progress.
-                elseif offset == q_start or offset == q_end then
-                    cur_hl = (offset == q_start and HL_START:format(hl_type)
-                            or HL_END)
-                    local part = piece:sub(1, 1)
-                    io.stdout:write(cur_hl .. part)
-                    piece = piece:sub(2)
-                    offset = offset + 1
-                -- Default case: output up until the next highlight change
-                else
-                    local bound = (offset < q_start and q_start or q_end)
-                    bound = math.min(bound - offset, #piece)
-                    local part = piece:sub(1, bound)
-                    io.stdout:write(part)
-                    piece = piece:sub(bound + 1)
-                    offset = offset + #part
-                end
-            end
-
-            -- Break out if this piece had a newline
-            if idx ~= nil then
-                -- Check for a highlight that ends on the newline
-                if offset == q_end then
-                    cur_hl = HL_END
-                end
-                break
+        -- Chop up this piece into parts that share the same highlight
+        while #piece > 0 do
+            -- Jump the cursor forward until we know the next match
+            if offset > q_end then
+                q_start, q_end = next_capture()
+            -- This piece starts or ends highlight. Output the escape code,
+            -- and one character of source so we make progress.
+            elseif offset == q_start or offset == q_end then
+                cur_hl = (offset == q_start and HL_START:format(hl_type)
+                        or HL_END)
+                local part = piece:sub(1, 1)
+                io.stdout:write(cur_hl .. part)
+                piece = piece:sub(2)
+                offset = offset + 1
+            -- Default case: output up until the next highlight change
+            else
+                local bound = (offset < q_start and q_start or q_end)
+                bound = math.min(bound - offset, #piece)
+                local part = piece:sub(1, bound)
+                io.stdout:write(part)
+                piece = piece:sub(bound + 1)
+                offset = offset + #part
             end
         end
 
-        io.stdout:write('\n')
+        -- Break out if this piece had a newline
+        if is_end then
+            -- Check for a highlight that ends on the newline
+            if offset == q_end then
+                cur_hl = HL_END
+            end
+            io.stdout:write('\n')
+        end
     end
 
     zmt.ts_query_cursor_delete(cursor)
