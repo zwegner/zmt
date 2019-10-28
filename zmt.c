27fce43067249f6ab5c3ab34e971d494ad8f5d57
Zach Wegner <zwegner@gmail.com>
2019-10-27 21:39:49 -0500

    Add hole and filler nodes, to support easy O(1) editing. Also, clean up some things and add a bunch of documentation in zmt.h.

--- zmt.c
+++ zmt.c
@@ -10,6 +10,24 @@
 // XXX kinda dumb
 chunk_t *current_chunk;
 
+// Read-only singleton, since it can be used everywhere. static const should
+// make this actually read only, with any writes causing a bus error.
+// We do make non-const pointers to this though, so we have to be careful.
+static const meta_node_t HOLE_NODE_SINGLETON = {
+    .flags = NODE_HOLE,
+    // We use non-zero byte/nl counts here, so that the hole takes up space in
+    // the metadata tree, and can thus be jumped to.
+    .byte_count = 1,
+    .nl_count = 1,
+    .leaf = {
+        // This should never actually be returned from the iterator protocol,
+        // so don't use any data
+        .chunk_data = NULL,
+        .start = 0,
+        .end = 1,
+    },
+};
+
 ////////////////////////////////////////////////////////////////////////////////
 // Chunks / Files //////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -57,6 +75,9 @@ chunk_t *map_file(const char *path) {
 meta_tree_t *create_tree() {
     meta_tree_t *tree = (meta_tree_t *)calloc(1, sizeof(meta_tree_t));
     tree->ref_count = 1;
+
+    // Flag the embedded filler node
+    tree->filler_node->flags = NODE_FILLER;
     return tree;
 }
 
@@ -86,7 +107,7 @@ static meta_node_t *duplicate_node(meta_node_t *node) {
 // Set any metadata from a chunk
 // Only call this on a new node, as the tree nodes should be immutable
 static void set_leaf_meta_data(meta_node_t *node) {
-    assert(node->flags & NODE_LEAF);
+    assert(node->flags & (NODE_LEAF | NODE_FILLER));
     node->byte_count = node->leaf.end - node->leaf.start;
     node->nl_count = 0;
     for (uint32_t i = node->leaf.start; i < node->leaf.end; i++)
@@ -109,8 +130,11 @@ static void set_inner_meta_data(meta_node_t *node) {
 void verify_node(meta_node_t *node) {
     uint32_t old_byte_count = node->byte_count;
     int32_t old_nl_count = node->nl_count;
-    if (node->flags & NODE_LEAF)
+    if (node->flags & (NODE_LEAF | NODE_FILLER))
         set_leaf_meta_data(node);
+    else if (node->flags & NODE_HOLE)
+        // XXX no-op
+        ;
     else {
         for (int32_t x = 0; x < MAX_CHILDREN; x++)
             if (node->inner.children[x])
@@ -175,6 +199,36 @@ meta_tree_t *dumb_read_data(chunk_t *chunk) {
     return tree;
 }
 
+meta_node_t *make_slice_node(meta_iter_t *iter, meta_node_t *node,
+        uint64_t offset) {
+    *iter->slice_node = *node;
+    iter->slice_node->leaf.start += offset;
+    // Re-initialize metadata. Maybe we should have an option to not
+    // recalculate this if it's not needed...?
+    set_leaf_meta_data(iter->slice_node);
+    return iter->slice_node;
+}
+
+// Create a new tree without a hole by replacing the hole node with
+// the filler node
+meta_tree_t *patch_tree_hole(meta_tree_t *tree) {
+    assert(tree->has_hole);
+
+    // Jump an iterator to the start of the hole
+    meta_iter_t iter[1];
+    meta_node_t *hole = iter_start(iter, tree, tree->hole_offset.byte, 0);
+    assert(hole == tree->filler_node);
+    assert(hole->flags & NODE_FILLER);
+
+    // Replace the hole with the filler node
+    meta_node_t *filler = duplicate_node(tree->filler_node);
+    filler->flags = NODE_LEAF;
+    tree = replace_current_node(iter, filler, false);
+    tree->has_hole = false;
+
+    return tree;
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Iteration ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -204,7 +258,14 @@ void iter_init(meta_iter_t *iter, meta_tree_t *tree) {
         goto next_iter;                                               \
     } while (0)
 
-#define YIELD(node)   do { iter->depth--; return (node); } while (0)
+#define YIELD(node)                                                   \
+    do {                                                              \
+        iter->start_offset = iter->end_offset;                        \
+        iter->end_offset.byte += (node)->byte_count;                  \
+        iter->end_offset.line += (node)->nl_count;                    \
+        iter->depth--;                                                \
+        return (node);                                                \
+    } while (0)
 
 #define RETURN()      do { iter->depth--; goto next_iter; } while (0)
 
@@ -216,13 +277,14 @@ meta_node_t *iter_next(meta_iter_t *iter) {
 
         switch (frame->state) {
             case ITER_START:
-                // Found leaf node?
-                if (node->flags & NODE_LEAF) {
-                    iter->start_offset = iter->end_offset;
-                    iter->end_offset.byte += node->byte_count;
-                    iter->end_offset.line += node->nl_count;
+                // Check for a leaf node
+                if (node->flags & NODE_LEAF)
                     YIELD(node);
-                }
+
+                // Check if this is a hole node. If so, yield the filler node
+                if (node->flags & NODE_HOLE)
+                    YIELD(iter->tree->filler_node);
+
                 frame->state = ITER_CHILDREN;
 
                 // FALLTHROUGH
@@ -238,13 +300,11 @@ meta_node_t *iter_next(meta_iter_t *iter) {
                 RETURN();
 
             case ITER_JUMP:
-                // Check for a leaf node. This is duplicated from above...
-                if (node->flags & NODE_LEAF) {
-                    iter->start_offset = iter->end_offset;
-                    iter->end_offset.byte += node->byte_count;
-                    iter->end_offset.line += node->nl_count;
+                // Check for a leaf node or hole node, like ITER_START above
+                if (node->flags & NODE_LEAF)
                     YIELD(node);
-                }
+                if (node->flags & NODE_HOLE)
+                    YIELD(iter->tree->filler_node);
 
                 // Reset the frame state to ITER_CHILDREN, since in JUMP mode
                 // we only affect the first descent into the tree--all the
@@ -281,17 +341,9 @@ next_iter:
     return NULL;
 }
 
-meta_node_t *make_dummy_node(meta_iter_t *iter, meta_node_t *node,
-        uint64_t offset) {
-    iter->dummy = *node;
-    iter->dummy.leaf.start += offset;
-    // Re-initialize metadata. Maybe we should have an option to not
-    // recalculate this if it's not needed...?
-    set_leaf_meta_data(&iter->dummy);
-    return &iter->dummy;
-}
-
-// Initialize an iterator object, and return the first node
+// Initialize an iterator object, and return the first node, starting at
+// either <byte_offset> bytes or <line_offset> lines into the file.
+// Only one of byte_offset/line_offset can be non-zero.
 meta_node_t *iter_start(meta_iter_t *iter, meta_tree_t *tree,
         uint64_t byte_offset, uint64_t line_offset) {
     iter_init(iter, tree);
@@ -301,6 +353,18 @@ meta_node_t *iter_start(meta_iter_t *iter, meta_tree_t *tree,
         // the desired offset
         iter->frame[0].state = ITER_JUMP;
         iter->desired_offset = (offset_t) { byte_offset, line_offset };
+
+        // See if we're jumping inside or past the hole. If so, we fake the offset
+        // to the iterator jump, since the size of the hole is not updated in the
+        // tree metadata.
+        if (tree->has_hole) {
+            // Subtract one from the actual size of the filler node, since the
+            // hole takes up one byte/newline of space in the metadata tree.
+            if (byte_offset > tree->hole_offset.byte)
+                iter->desired_offset.byte -= tree->filler_node->byte_count - 1;
+            if (line_offset > tree->hole_offset.line)
+                iter->desired_offset.line -= tree->filler_node->nl_count - 1;
+        }
 
         meta_node_t *node = iter_next(iter);
         if (!node)
@@ -340,16 +404,18 @@ meta_node_t *iter_start(meta_iter_t *iter, meta_tree_t *tree,
             }
         }
 
-        // XXX Only update byte count here, since that's the only item used
-        // externally right now. Do we want to update line number? That
-        // can be much more expensive than this, needing to scan over all
-        // of the bytes we skipped over...
-        iter->start_offset.byte += offset;
-
         // The desired byte offset is somewhere in the middle. Rather than
         // try to make a weird API to pass this information to the caller,
-        // create a dummy node with just the slice we want
-        return make_dummy_node(iter, node, offset);
+        // create a slice node with just the slice we want
+        meta_node_t *slice = make_slice_node(iter, node, offset);
+
+        // Update the offset. The byte is simple, but for newlines we use
+        // a delta between the old and new nodes so we don't have to scan
+        // both sides of the split.
+        iter->start_offset.byte += offset;
+        iter->start_offset.line += node->nl_count - slice->nl_count;
+
+        return slice;
     }
 
     // Default case: just start the normal iteration
@@ -362,8 +428,23 @@ meta_node_t *iter_start(meta_iter_t *iter, meta_tree_t *tree,
 // around so we have the full path from root to leaf.
 // This modifies the iterator in-place to continue iteration from the new
 // tree.
-meta_tree_t *replace_current_node(meta_iter_t *iter, meta_node_t *new_node) {
+meta_tree_t *replace_current_node(meta_iter_t *iter, meta_node_t *new_node,
+        bool create_hole) {
     meta_tree_t *tree = create_tree();
+    // Is the new node a hole node (or a parent of one)?
+    if (create_hole) {
+        // If there is already a hole in this tree, patch it up. We don't
+        // bother if we're replacing the filler node.
+        if (tree->has_hole &&
+                !(iter->frame[iter->depth].node->flags & NODE_HOLE)) {
+            // This is recursive, but can only go one deep
+            // XXX This tree will be immediately replaced. Any optimization
+            // possible here? Or just let gc take care of it?
+            tree = patch_tree_hole(tree);
+        }
+        tree->has_hole = true;
+        tree->hole_offset = iter->start_offset;
+    }
 
     // Walk back up the stack, copying each parent node in succession
     for (uint32_t depth = iter->depth; depth > 0; depth--) {
@@ -395,26 +476,97 @@ meta_tree_t *replace_current_node(meta_iter_t *iter, meta_node_t *new_node) {
     return tree;
 }
 
-// XXX dumb
-meta_tree_t *mangle_tree(meta_tree_t *tree, uint64_t offset,
+// Like replace_current_node() above, but splits a node into two at the current
+// iteration offset, and creates a hole between them. We take the current node
+// as a parameter, since we don't generally store the just-yielded node inside
+// the iterator, and we frequently jump into the middle of nodes and create
+// slice nodes, so we can't easily derive it. We can compare the yielded node
+// with the node at the top of the iteration tree, and thus find how far into
+// the latter node we need to split.
+meta_tree_t *split_current_node(meta_iter_t *iter, meta_node_t *node) {
+    meta_node_t *parent = create_node();
+
+    // There are two main cases to handle here, based on whether the jump
+    // offset started perfectly on a node or not. Right now we can always
+    // determine this based on whether we got the slice node from the iterator.
+    if (node == iter->slice_node) {
+        assert(iter->depth > 0);
+        // Get the base node that the current node is a slice of
+        // This is a tiny hack, in that it looks into the stack frame that
+        // just got "popped", to see the node that is current
+        meta_iter_frame_t *frame = &iter->frame[iter->depth];
+        meta_node_t *base_node = frame->node;
+        if (base_node->flags & NODE_HOLE) {
+            assert(iter->tree->has_hole);
+            base_node = iter->tree->filler_node;
+        }
+
+        // Sanity check that this is the right base node
+        assert(node->flags & (NODE_LEAF | NODE_FILLER));
+        assert(base_node->flags & (NODE_LEAF | NODE_FILLER));
+        assert(node->leaf.chunk_data == base_node->leaf.chunk_data);
+        assert(node->leaf.start > base_node->leaf.start);
+        assert(node->leaf.end == base_node->leaf.end);
+
+        // Create two nodes: one with the bytes to the right of the current
+        // offset, which we can copy directly from the current node, and one
+        // with the bytes to the left, which we create by comparing the
+        // current node with its base
+        meta_node_t *child_r = duplicate_node(node);
+        meta_node_t *child_l = duplicate_node(base_node);
+        child_l->leaf.end = child_r->leaf.start;
+        child_l->byte_count -= child_r->byte_count;
+        child_l->nl_count -= child_r->nl_count;
+
+        // XXX This logic is MAX_CHILDREN==2 specific
+        meta_node_t *sub_parent = create_node();
+        sub_parent->inner.children[0] = child_l;
+        sub_parent->inner.children[1] = (meta_node_t *)&HOLE_NODE_SINGLETON;
+        set_inner_meta_data(sub_parent);
+
+        parent->inner.children[0] = sub_parent;
+        parent->inner.children[1] = child_r;
+        set_inner_meta_data(parent);
+    }
+    // Not the slice node: we are making a hole at the boundary between
+    // two nodes. This case is a lot simpler, we just create one parent
+    // with the hole on the left and the current node on the right.
+    else {
+        assert(node->flags & (NODE_LEAF | NODE_FILLER));
+        // XXX This logic is MAX_CHILDREN==2 specific
+        parent->inner.children[0] = (meta_node_t *)&HOLE_NODE_SINGLETON;
+        parent->inner.children[1] = node;
+        set_inner_meta_data(parent);
+    }
+
+    return replace_current_node(iter, parent, true);
+}
+
+// Helper function: split the tree at the given offset, and insert the
+// given bytes into the filler node
+meta_tree_t *insert_bytes_at_offset(meta_tree_t *tree, uint64_t offset,
         const uint8_t *data, uint64_t len) {
     // Copy data into chunk
+    // XXX chunk management
     if (!current_chunk)
         current_chunk = create_chunk(NULL, 0x10000);
     assert(current_chunk->used + len < current_chunk->len);
     memcpy((uint8_t *)&current_chunk->data[current_chunk->used], data, len);
 
-    meta_node_t *new_node = create_leaf();
+    // XXX this is suboptimal, we should check for appending to the
+    // filler node
+    meta_iter_t iter[1];
+    meta_node_t *old_node = iter_start(iter, tree, offset, 0);
+    tree = split_current_node(iter, old_node);
+
+    meta_node_t *new_node = tree->filler_node;
     new_node->leaf.chunk_data = current_chunk->data;
     new_node->leaf.start = current_chunk->used;
     new_node->leaf.end = current_chunk->used + len;
     set_leaf_meta_data(new_node);
     current_chunk->ref_count++;
 
-    meta_iter_t iter[1];
-    (void)iter_start(iter, tree, offset, 0);
-
-    return replace_current_node(iter, new_node);
+    return tree;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- zmt.h
+++ zmt.h
@@ -3,28 +3,115 @@
 
 #include "tree_sitter/api.h"
 
-// Flags
-enum {
-    NODE_INNER = 1,
-    NODE_LEAF = 2
-};
+// ===META TREES===
+// The meta-tree structure is the core of this editor. It's a combination
+// of piece tables, persistent trees, and a zipper/one-hole context. Here's
+// some quick references for these concepts:
+//      https://en.wikipedia.org/wiki/Piece_table
+//      https://en.wikipedia.org/wiki/Persistent_data_structure#Trees
+//      https://en.wikipedia.org/wiki/Zipper_(data_structure)
+//
+// What does this mean? First off, all of the actual bytes of data in a buffer
+// are stored in append-only chunks of data. When new text is inserted into the
+// buffer, that new text is added to a data block, and the tree representing
+// the current buffer state will get a new node that references the data block,
+// with the appropriate offsets. Deleting text from a buffer only involves meta
+// data updates: no new text gets added/removed from data blocks, but some
+// nodes of the tree will get modified/removed to reflect the new state of the
+// buffer.
+//
+// So that's the piece table part (or I guess "piece tree"). The next step is
+// persistency. Persistency means that the piece tree representing the buffer
+// is actually an immutable snapshot: when we make edits to the tree, we return
+// a brand new tree, and the old tree is still valid. Since generally most of
+// the tree doesn't need to be edited, these trees can share many subtrees. In
+// fact, when replacing a node inside a tree at depth D, only D new nodes need
+// to be created. Thus if the tree is reasonably balanced, editing operations
+// can be completed in O(log N) time, with N being the number of tree nodes,
+// and the base of the log determined by the branching factor of the tree (in
+// this code, MAX_CHILDREN). Why does persistency matter at all here? One big
+// benefit is thread safety: we can do things like pass one version of the
+// buffer off to a syntax highlighter in a separate thread, and continue
+// editing while the buffer is being parsed. Another big benefit is having
+// instant access to all versions of the buffer throughout history, in parallel.
+// This will eventually allow wacky features like showing different snapshots
+// of a buffer side-by-side or searching for a regular expression through all
+// historical versions of a buffer (hopefully without needing to search the
+// entire buffer for every snapshot).
+//
+// So, O(log N) is pretty good, and edits should generally complete in something
+// like a microsecond. But that's not quite good enough for me. That's where
+// the one-hole context comes in. This isn't used with all the type system
+// magic as in functional programming, and thus doesn't have the same
+// flexibility or range of benefits, but that's where the idea came from. It's
+// pretty much just used for one thing: making lots of sequential edits faster.
+// It can also be thought of as an adaptation of the "gap buffer" idea to the
+// tree structure. When editing starts at a particular place in the buffer,
+// we split the metadata tree at that point, and insert a hole. The hole is
+// basically an indirection, a marker to graft a different subtree in at this
+// point, called the filler. As editing continues (assuming only inserting
+// characters in sequence), the filler node is edited, while the rest of the
+// tree can remain untouched. If no other threads hold a reference to the tree
+// snapshot, the filler node can be updated in place. Otherwise, just one new
+// tree is allocated (which holds the filler node inline), rather than needing
+// to allocate tree nodes for the entire path leading down to the edit point.
+// Either way, sequential edits are done in O(1) time instead of O(log N).
+//
+// At the moment, only one hole is supported, and the filler node is always
+// a leaf node. It would be fairly straightforward to support multiple holes
+// (which would work very nicely for multiple cursor support a la Lapis/Sublime
+// Text). Larger filler sub-trees would be feasible as well, but as the size
+// of the filler gets larger, the benefit starts to disappear.
 
-#define MAX_CHILDREN            (2)
-#define DUMB_MAX_CHUNKS         (16)
-#define ITER_STACK_SIZE         (32)
+typedef struct {
+    // XXX make these 32-bit for now for luajit compat. This size is inadequate!
+    uint32_t byte;
+    uint32_t line;
+} offset_t;
 
 // chunk_t holds big chunks of contiguous backing memory
-// XXX make variant with embedded data for non-mmap()ed memory, so we only
-// need one allocation per chunk
+
+#define CHUNK_EMBEDDED          (1 << 0)
 typedef struct {
     uint32_t ref_count;
+    uint32_t flags;
     const uint8_t *data;
     size_t len;
     size_t used;
+    const uint8_t embedded_data[];
 } chunk_t;
 
-// Main tree node for storing buffer state. We use one node type with a union,
-// so we can use the same pointer types everywhere
+// Main tree node structure for storing buffer state. We use one node type
+// with a union, so we can use the same pointer types everywhere. The flags
+// field has bits for determining which node type it is, according to the
+// enum below.
+//
+// The overall size of the node involves a few tradeoffs/constraints:
+//  * the size of leaf and inner nodes should ideally be the same size, so
+//      that neither node type wastes any space.
+//  * MAX_CHILDREN is the base of the log in our O(log N). Having more
+//      children will reduce the overall depth of the tree, but this isn't
+//      a pure win, since all of the re-allocating in creating a new tree
+//      involves copying proportional to MAX_CHILDREN.
+//  * Using MAX_CHILDREN > 2 might complicate some code paths involved in
+//      tree editing
+//  * the metadata that is stored only in leaf nodes should only be locally
+//      useful (say, for rendering text). We store byte and newline counts
+//      for leaf and internal nodes because they are needed for efficiently
+//      seeking through the file.
+//
+// Right now, with no metadata in leaf nodes beyond byte/newline count,
+// we can use MAX_CHILDREN==2 and fit a node into a nice 32 bytes.
+
+#define MAX_CHILDREN            (2)
+
+// Flags
+enum { node_inner_bit, node_leaf_bit, node_hole_bit, node_filler_bit };
+#define NODE_INNER              (1 << node_inner_bit)
+#define NODE_LEAF               (1 << node_leaf_bit)
+#define NODE_HOLE               (1 << node_hole_bit)
+#define NODE_FILLER             (1 << node_filler_bit)
+
 typedef struct meta_node_t {
     uint32_t ref_count;
     uint32_t flags;
@@ -40,20 +127,37 @@ typedef struct meta_node_t {
             uint32_t end;
             // Store a pointer to the raw data, rather than the chunk_t.
             // This is just for efficiency, since we don't want an extra
-            // level of indirection for editing. We keep a map of chunks
-            // used within each tree in meta_tree_t so we can access the
-            // metadata if we need it.
+            // level of indirection for most read operations. We keep a
+            // map of chunks used within each tree in meta_tree_t so we
+            // can access the metadata if we need it.
             const uint8_t *chunk_data;
         } leaf;
     };
 } meta_node_t;
 
 // Root structure for storing one snapshot of a buffer state, with any extra
-// data needed. We store a list of chunks referenced in the tree
+// data needed. We store a list of chunks referenced in the tree.
+#define DUMB_MAX_CHUNKS         (16)
 typedef struct {
     uint32_t ref_count;
     meta_node_t *root;
+
+    // Store whether this tree has a hole. It can only have one.
+    bool has_hole;
+
+    // The offset of the hole from the beginning of the file. This is needed
+    // because tree metadata doesn't take the filler node into account, and
+    // thus we need to know where the hole is, so we can take the size of
+    // the filler into account when seeking past it
+    offset_t hole_offset;
+
+    // This node fills in the hole, if there is one
+    meta_node_t filler_node[1];
+
     // XXX actual map of chunk data->chunk
+    // XXX also need to store a local refcount here--to incrementally maintain
+    // this map, and delete items from it when they're unused, we don't want
+    // to have to walk the whole tree
     chunk_t *chunks[DUMB_MAX_CHUNKS];
     uint32_t chunk_count;
 } meta_tree_t;
@@ -69,12 +173,6 @@ enum ITER_STATE {
     ITER_JUMP,
 };
 
-typedef struct {
-    // XXX make these 32-bit for now for luajit compat. This size is inadequate!
-    uint32_t byte;
-    uint32_t line;
-} offset_t;
-
 // Stack frame for iterative tree walking
 typedef struct {
     meta_node_t *node;
@@ -83,6 +181,12 @@ typedef struct {
 } meta_iter_frame_t;
 
 // Main iterator structure
+
+// XXX right now we have a hardcoded stack limit. 32 is pretty big as far as
+// number of edits, MAX_CHILDREN^32 nodes is a lot of editing, if we can keep
+// our trees balanced.
+#define ITER_STACK_SIZE         (32)
+
 typedef struct {
     meta_tree_t *tree;
 
@@ -90,15 +194,23 @@ typedef struct {
     meta_iter_frame_t frame[ITER_STACK_SIZE];
     int32_t depth;
 
+    // These offsets are set during iteration to the start/end points of
+    // each leaf node.
+    // XXX User code should never touch these... Maybe we should make a
+    // function and keep these "private"
     offset_t start_offset;
     offset_t end_offset;
 
+    // The offset we're currently seeking towards
     offset_t desired_offset;
 
-    meta_node_t dummy;
+    meta_node_t slice_node[1];
 } meta_iter_t;
 
-// Prototypes
+////////////////////////////////////////////////////////////////////////////////
+// Prototypes //////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
 chunk_t *map_file(const char *path);
 meta_tree_t *read_data(chunk_t *chunk);
 void verify_node(meta_node_t *node);
@@ -110,13 +222,13 @@ meta_node_t *iter_start(meta_iter_t *iter, meta_tree_t *tree,
 meta_node_t *iter_next(meta_iter_t *iter);
 
 // Mutation
-meta_tree_t *replace_current_node(meta_iter_t *iter, meta_node_t *new_node);
+meta_tree_t *replace_current_node(meta_iter_t *iter, meta_node_t *new_node,
+        bool create_hole);
 
-// XXX remove this
-meta_tree_t *mangle_tree(meta_tree_t *tree, uint64_t offset,
+// XXX experimental/test apis
+meta_tree_t *patch_tree_hole(meta_tree_t *tree);
+meta_tree_t *insert_bytes_at_offset(meta_tree_t *tree, uint64_t offset,
         const uint8_t *data, uint64_t len);
-
-// XXX remove this
 meta_tree_t *dumb_read_data(chunk_t *chunk);
 
 // Tree-sitter stuff
